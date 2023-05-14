15fd13397286c7a6eb8abb40d4fde14e14addba4
Maksym Zavalniuk <mezgoodle@gmail.com>
2023-05-14 07:04:47 +0000

    Update eslint ignore

--- index.test.js
+++ index.test.js
@@ -17,8 +17,8 @@ test('wait 500 ms', async () => {
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_GITLABTOKEN'] = "gitlab token";
-  process.env['INPUT_BITBUCKETTOKEN'] = "bitbucket token";
+  process.env['INPUT_GITLABTOKEN'] = 'gitlab token';
+  process.env['INPUT_BITBUCKETTOKEN'] = 'bitbucket token';
   const ip = path.join(__dirname, 'index.js');
   const result = cp.execSync(`node ${ip}`, {env: process.env}).toString();
   console.log(result);
