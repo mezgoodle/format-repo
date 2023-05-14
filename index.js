8a752de5a8661842cd36dc6c63ef4801f6e7742d
Maksym Zavalniuk <mezgoodle@gmail.com>
2023-05-14 14:30:22 +0000

    Build workflow

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec');
 
-try {
+const mainFunc = async () => {
   let myOutput = '';
   let myError = '';
 
@@ -35,6 +35,10 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
+};
+
+try {
+  mainFunc();
 } catch (error) {
   core.setFailed(error.message);
 }
