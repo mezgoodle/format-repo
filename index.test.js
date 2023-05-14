f031f8a3752b3c01d78ea5ceab376b4207d7e149
Maksym Zavalniuk <mezgoodle@gmail.com>
2023-05-14 07:02:17 +0000

    Add test coverage

---
+++ index.test.js
@@ -0,0 +1,25 @@
+const wait = require('./wait');
+const process = require('process');
+const cp = require('child_process');
+const path = require('path');
+
+test('throws invalid number', async () => {
+  await expect(wait('foo')).rejects.toThrow('milliseconds not a number');
+});
+
+test('wait 500 ms', async () => {
+  const start = new Date();
+  await wait(500);
+  const end = new Date();
+  const delta = Math.abs(end - start);
+  expect(delta).toBeGreaterThanOrEqual(500);
+});
+
+// shows how the runner will run a javascript action with env / stdout protocol
+test('test runs', () => {
+  process.env['INPUT_GITLABTOKEN'] = "gitlab token";
+  process.env['INPUT_BITBUCKETTOKEN'] = "bitbucket token";
+  const ip = path.join(__dirname, 'index.js');
+  const result = cp.execSync(`node ${ip}`, {env: process.env}).toString();
+  console.log(result);
+});
