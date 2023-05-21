9576795b3d92e4e0f726a85d89763c3acb94fd0f
Maksym Zavalniuk <mezgoodle@gmail.com>
2023-05-21 16:56:59 +0000

    Fix push command

--- dist/index.js
+++ dist/index.js
@@ -10897,7 +10897,11 @@ const gitAction =async (payload, token) => {
   await exec.exec(`git config --global user.name ${payload.pusher.name}`);
   await exec.exec(`git config --global user.email  ${payload.pusher.email}`);
   await exec.exec('git commit -am "Automated format"', [], options);
-  await exec.exec(`git push https://oauth2:${token}@github.com/${payload.repository.full_name}.git`, [], options);
+  try {
+    await exec.exec(`git push https://oauth2:${token}@github.com/${payload.repository.full_name}.git`, [], options);
+  } catch (error) {
+    console.log("Error with push");
+  }
 };
 
 module.exports = {gitAction};
